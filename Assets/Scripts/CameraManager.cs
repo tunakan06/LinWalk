947558b7cc27245e38c82b917c9cffbd4a461a47
Takamasa Kambe <kombe20@gmail.com>
2022-11-08 21:01:29 +0900

    UnityProject_20220923 first commit

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+
+    //プレイヤーを格納する変数
+    public GameObject player;
+
+    //プレイヤーとの距離を調整する変数
+    public float offsetx;
+    public float offsety;
+    public float offsetz;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //プレイヤーのポジション
+        //Vector3 pos = player.transform.position;
+
+        //カメラのポジション
+        //transform.position = new Vector3(pos.x + offsetx, pos.y + offsety, pos.z + offsetz);
+
+        //左シフトが押されている時
+        if (Input.GetKey(KeyCode.Q))
+        {
+            //ユニティちゃんを中心に-5f度回転
+            transform.RotateAround(player.transform.position, Vector3.up, -5f);
+        }
+        //右シフトが押されている時
+        else if (Input.GetKey(KeyCode.E))
+        {
+            //ユニティちゃんを中心に5f度回転
+            transform.RotateAround(player.transform.position, Vector3.up, 5f);
+        }
+
+    }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private CharacterController controller;
+    [SerializeField] Animator animator;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private float playerSpeed = 3.0f;
+    private float playerRunSpeed = 5.0f;
+    private float jumpHeight = 1.0f;
+    private float gravityValue = -9.81f;
+
+    private void Start()
+    {
+        controller = gameObject.AddComponent<CharacterController>();
+        controller.center = new Vector3(0, 1, 0);
+        controller.radius = 0.25f;
+        controller.height = 1.85f;
+
+        //animator = gameObject.AddComponent<Animator>();
+    }
+
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        // 走る
+        if (Input.GetKey("left shift") &&(move.magnitude > 0))
+        {
+            controller.Move(move * Time.deltaTime * playerSpeed);
+            animator.SetBool("Walking", true);
+            animator.SetBool("Running", false);
+        }
+        // 歩く
+        else if((move.magnitude > 0))
+        {
+            controller.Move(move * Time.deltaTime * playerRunSpeed);
+            animator.SetBool("Running", true);
+            animator.SetBool("Walking", false);
+        }
+        // 止まる
+        else
+        {
+            //controller.Move(move * Time.deltaTime * playerSpeed);
+            animator.SetBool("Walking", false);
+            animator.SetBool("Running", false);
+        }
+        
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+
+        // 行動
+        // ジャンプ
+        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+        // お辞儀
+        if (Input.GetKey(KeyCode.Z) && (move.magnitude == 0.0f))
+        {
+            animator.SetBool("Bowing", true);
+        }
+        else
+        {
+            animator.SetBool("Bowing", false);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+}
